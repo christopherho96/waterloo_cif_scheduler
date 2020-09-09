c0c1a85628149ccb3bc13832f8e3073440a1192e
Christopher Ho <ho.christopher.tak@gmail.com>
2020-09-08 23:08:41 -0400

    Error check for twilio text error of 1600 chars

--- scheduler.py
+++ scheduler.py
@@ -29,16 +29,25 @@ for schedule_card in schedule_cards:
         details = schedule_card.find('small').text.split("\n")
         time = details[1].strip()
         open_spots = details[3].strip()
-        message += "\n{}\n{}\n{}\n".format(date, time, open_spots)
+        if open_spots != "No Spots Available":
+            message += "\n{}\n{}\n{}\n".format(date, time, open_spots)
     except Exception as e:
         logging.error(e)
-        logging.error("Error web scraping for each reservation detail on CIF schdeule")
+        logging.error("Error web scraping for each reservation detail on CIF schedule")
         sys.exit()
 
-message += "\nBook gym reservation now at https://warrior.uwaterloo.ca/Program/GetProgramDetails?courseId=cc2a16d7-f148-461e-831d-7d4659726dd1&semesterId=b0d461c3-71ea-458e-b150-134678037221 "
+if message == "":
+    message = "\nNo available gym reservations\n\n{}".format(url)
+else:
+    message += "\nBook CIF gym reservation now at\n\n{}".format(url)
 try:
-    client.messages.create(body=message, from_=twilio_phone_number, to=personal_phone_number)
-    logging.info("Successfully sent text")
+    if len(message) >= 1600:
+        error = "Message is longer than 1600 characters. Click link to see CIF schedule\n\n {}".format(url)
+        logging.error(error)
+        client.messages.create(body=error, from_=twilio_phone_number, to=personal_phone_number)
+    else:
+        client.messages.create(body=message, from_=twilio_phone_number, to=personal_phone_number)
+        logging.info("Successfully sent text")
 except Exception as e:
     logging.error(e)
     logging.error("Error sending text message")
